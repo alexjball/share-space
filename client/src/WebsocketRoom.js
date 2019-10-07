6b879b123154810641a53cc87f6171052c0b6935
Alex Ball <ball.alex.509@gmail.com>
2019-10-07 15:42:34 -0400

    Working webm streaming

--- client/src/WebsocketRoom.js
+++ client/src/WebsocketRoom.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import "./Room.css";
 
-const mimeType = 'video/mp4; codecs="avc1.42001e, mp4a.67"';
+// const mimeType = 'video/mp4; codecs="avc1.42001e, mp4a.67"';
+const mimeType = 'video/webm; codecs="vp9"';//,vorbis"';
 
 /**
  * The main interface to spaces, using a websocket.
@@ -36,6 +37,24 @@ export default class WebsocketRoom extends Component {
     };
     ws.onmessage = event => {
       if (this.sourceBuffer) {
+        const tr = this.sourceBuffer.buffered;
+        const printTr = (tr) => {
+          if (tr.length) {
+            return `${tr.start(0)} ${tr.end(0)}`;
+          }
+          return "empty TimeRanges";
+        };
+        if (this.videoRef.current.error) {
+          console.log(this.videoRef.current.error);
+        }
+        if (tr.length && this.videoRef.current) {
+          console.log(printTr(tr), this.videoRef.current.currentTime, printTr(this.videoRef.current.seekable));
+        }
+        // console.log(this.sourceBuffer.buffered.start(0), this.sourceBuffer.buffered.end(0));
+        if (this.sourceBuffer.updating) {
+          console.warn("sourceBuffer is updating, aborting");
+          this.sourceBuffer.abort();
+        }
         this.sourceBuffer.appendBuffer(event.data);
       }
       this.setState({
@@ -43,7 +62,7 @@ export default class WebsocketRoom extends Component {
       });
     };
 
-    const videoSource = new MediaSource();
+    const videoSource = (this.videoSource = new MediaSource());
     videoSource.addEventListener("sourceopen", () => {
       this.sourceBuffer = videoSource.addSourceBuffer(mimeType);
     });
@@ -69,7 +88,14 @@ export default class WebsocketRoom extends Component {
         <div>{`spaceUrl: ${this.props.spaceUrl}`}</div>
         <div>{`status: ${this.state.status}`}</div>
         <div>{`latest message time: ${this.state.lastMessageTime}`}</div>
-        <video autoPlay={false} ref={this.videoRef} className="video" />
+        <video autoPlay={true} ref={this.videoRef} className="video" />
+        <button
+          onClick={() => {
+            this.videoRef.current.play();
+          }}
+        >
+          Play
+        </button>
       </div>
     );
   }
